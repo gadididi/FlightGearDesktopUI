a9689a319268d94d4eb3925e58870a1f605067dd
Ori Levy <ori294@gmail.com>
2020-03-17 18:27:15 +0200

    Added the view to the main window

--- FlightSimulator/FlightSimulatorApp/view/Joystick.xaml.cs
+++ FlightSimulator/FlightSimulatorApp/view/Joystick.xaml.cs
@@ -19,9 +19,59 @@ namespace FlightSimulatorApp.view
     /// 
     public partial class Joystick : UserControl
     {
+        private Point _positionInBlock;
+        bool first_time = true;
+
         public Joystick()
         {
             InitializeComponent();
+        }
+
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // when the mouse is down, get the position within the current control. (so the control top/left doesn't move to the mouse position)
+            if (first_time) { 
+                _positionInBlock = Mouse.GetPosition(this);
+                first_time = false;
+            }
+            
+            // capture the mouse (so the mouse move events are still triggered (even when the mouse is not above the control)
+            this.CaptureMouse();
+        }
+
+        private void UserControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            // if the mouse is captured. you are moving it. (there is your 'real' boolean)
+            if (this.IsMouseCaptured)
+            {
+                // get the parent container
+                var container = this as UIElement;
+
+                // get the position within the container
+                var mousePosition = e.GetPosition(container);
+
+                double XPos = mousePosition.X - _positionInBlock.X;
+                double YPos = mousePosition.Y - _positionInBlock.Y;
+
+                // move the usercontrol.
+                if (XPos > -1)
+                {
+                    if (YPos < 1)
+                    {
+
+                    }
+                }
+                Knob.RenderTransform = new TranslateTransform(XPos, YPos);
+
+            }
+        }
+
+        private void UserControl_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // release this control.
+            this.ReleaseMouseCapture();
+            Knob.RenderTransform = new TranslateTransform();
         }
     }
 }
